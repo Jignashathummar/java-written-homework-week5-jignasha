f34fe265817beb7d66c6faeb13548af153779bcd
Jignashathummar <jignashakotadiya10@gmail.com>
2023-11-17 10:25:06 +0000

    Written homework week5

---
+++ src/homework_week_9/Programme_4_ColoursArrayList.java
@@ -0,0 +1,26 @@
+package homework_week_9;
+
+import java.util.ArrayList;
+
+/*
+Write a Java program to create a new array list, add some colours (string)
+and printout the collection using for each loop.
+ */
+public class Programme_4_ColoursArrayList {
+    public static void main(String[] args) {
+        ArrayList<String> colorlist = new ArrayList<>();
+        colorlist.add("RED");
+        colorlist.add("BLUE");
+        colorlist.add("GREEN");
+        colorlist.add("YELLOW");
+        colorlist.add("BLACK");
+        colorlist.add("WHITE");
+        colorlist.add("ORANGE");
+        colorlist.add("PURPLE");
+        colorlist.add("CYAN");
+
+        for (String colourList : colorlist) {
+            System.out.print(colourList + " , ");
+        }
+    }
+}
